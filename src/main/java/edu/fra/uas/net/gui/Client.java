c01f28ac15befd9d43e406a66af8db36293103b9
Harsh Mukhiya <ha981muk@gmail.com>
2022-06-16 19:50:16 +0200

    GUI: ConfigTab,ChatTab

--- src/main/java/edu/fra/uas/net/gui/Client.java
+++ src/main/java/edu/fra/uas/net/gui/Client.java
@@ -9,6 +9,9 @@ import java.awt.Color;
 import java.awt.Panel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+
+import edu.fra.uas.net.model.User;
+
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -17,53 +20,77 @@ import javax.swing.JComboBox;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JTabbedPane;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 
 public class Client {
+	
+	private static List<User> users =new ArrayList<>();
 
 	private JFrame frame;
-	private final Panel panelUser = new Panel();
+	private final Panel panelUserList = new Panel();
 	private final Panel panelMessage = new Panel();
-	private final Panel panelTextSendAndAdd = new Panel();
-	private final Panel panelSearch = new Panel();
-	private final JTextField textFieldSearch = new JTextField();
-	private final JButton btnNewButton = new JButton("Add File");
-	private final JButton btnNewButton_1 = new JButton("Send");
-	private final JButton btnNewButton_2 = new JButton("Create Group");
-	private final JButton btnNewButton_3 = new JButton(" Select Contacts");
-	private final JTextField textField = new JTextField();
-	private final Panel panel = new Panel();
-	private final JLabel lblNewLabel = new JLabel("Client's Name");
-	private final Panel panel_1 = new Panel();
-	private final JTextField textField_1 = new JTextField();
+	private final Panel panelUserSearch = new Panel();
+	private final JTextField textFieldUserSearch = new JTextField();
+	private final Panel panelClientsName = new Panel();
+	private final JLabel lblClientsName = new JLabel("  Client's Name");
 	private final Panel panelClients = new Panel();
-	private final Panel panel_2 = new Panel();
-	private final JLabel lblNewLabel_1 = new JLabel("  Client's Name 1");
-	private final JTextField textField_2 = new JTextField();
-	private final Panel panel_2_1 = new Panel();
-	private final JLabel lblNewLabel_1_1 = new JLabel("  Client's Name 2");
-	private final JTextField textField_3 = new JTextField();
-	private final Panel panel_2_2 = new Panel();
-	private final JLabel lblNewLabel_1_2 = new JLabel("  Client's Name 3");
-	private final JTextField textField_4 = new JTextField();
-	private final Panel panel_2_3 = new Panel();
-	private final JLabel lblNewLabel_1_3 = new JLabel("  Client's Name 4");
-	private final JTextField textField_5 = new JTextField();
-	private final Panel panel_2_4 = new Panel();
-	private final JLabel lblNewLabel_1_4 = new JLabel("  Client's Name 5");
-	private final JTextField textField_6 = new JTextField();
-	private final Panel panel_2_5 = new Panel();
-	private final JLabel lblNewLabel_1_5 = new JLabel("  Client's Name 6");
-	private final JTextField textField_7 = new JTextField();
-	private final Panel panel_2_6 = new Panel();
-	private final JLabel lblNewLabel_1_6 = new JLabel("  Client's Name 7");
-	private final JTextField textField_8 = new JTextField();
-	private final JButton btnNewButton_4 = new JButton("Search");
-	private final JMenuBar menuBar = new JMenuBar();
+	private final Panel panelUser = new Panel();
+	private final JLabel lbClientName = new JLabel("  Client's Name 1");
+	private final JButton btnUserSearch = new JButton("Search");
+	private final JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+	private final JPanel panelConfig = new JPanel();
+	private final JPanel panelChat = new JPanel();
+	private final JButton btnSelectContacts = new JButton(" Select Contacts");
+	private final JButton btnCreateGroup = new JButton("Create Group");
+	private final JLabel lbClientLastMessage = new JLabel("Typping...");
+	private final JScrollPane scrollPane = new JScrollPane();
+	private final JPanel panelConfigServerUser = new JPanel();
+	private final JButton btnSave = new JButton("Save");
+	private final JLabel lblServerIp = new JLabel("IP");
+	private final JLabel lblServerPort = new JLabel("Port");
+	private final JTextField textFieldServerIp = new JTextField();
+	private final JTextField textFieldServerPort = new JTextField();
+	private final Panel panelConfigServer = new Panel();
+	private final JLabel lblServer = new JLabel("Server");
+	private final Panel panelConfigUser = new Panel();
+	private final JLabel lblUserIp = new JLabel("IP");
+	private final JTextField textFieldUserIp = new JTextField();
+	private final JLabel lblUserPort = new JLabel("Port");
+	private final JTextField textFieldUserPort = new JTextField();
+	private final JLabel lblUser = new JLabel("Username");
+	private final JTextField textFieldCurrentMessage = new JTextField();
+	private final JButton btnMessageSend = new JButton("Send");
+	private final JButton btnFileAdd = new JButton("Add File");
+	private final Panel panelMessageAddFileAndSend = new Panel();
+	private final JTextField textFieldPreviousMessage = new JTextField();
+	private final Panel panelUser_1 = new Panel();
+	private final JLabel lbClientName_1 = new JLabel((String) null);
+	private final JLabel lbClientLastMessage_1 = new JLabel("Typping...");
+	private final Panel panelUser_2 = new Panel();
+	private final JLabel lbClientName_2 = new JLabel((String) null);
+	private final JLabel lbClientLastMessage_2 = new JLabel("Typping...");
+	private final Panel panelUser_3 = new Panel();
+	private final JLabel lbClientName_3 = new JLabel((String) null);
+	private final JLabel lbClientLastMessage_3 = new JLabel("Typping...");
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		users.add(new User("client1", "localhost", 1010));
+		users.add(new User("client2", "localhost", 1011));
+		users.add(new User("client3", "localhost", 1012));
+		users.add(new User("client4", "localhost", 1013));
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -87,157 +114,191 @@ public class Client {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		textField_2.setBounds(6, 28, 287, 32);
-		textField_2.setColumns(10);
-		textField_1.setBounds(6, 6, 592, 462);
-		textField_1.setColumns(10);
-		textField.setBounds(6, 6, 451, 51);
-		textField.setColumns(10);
-		textFieldSearch.setBounds(6, 11, 236, 41);
-		textFieldSearch.setColumns(10);
+		textFieldPreviousMessage.setBounds(10, 79, 441, 334);
+		textFieldPreviousMessage.setColumns(10);
 		frame = new JFrame();
+		frame.setResizable(false);
 		frame.getContentPane().setForeground(Color.WHITE);
-		frame.setBounds(100, 100, 1004, 733);
+		frame.setBounds(100, 100, 872, 631);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		panelUser.setBackground(Color.WHITE);
-		panelUser.setBounds(0, 69, 352, 627);
+		tabbedPane.setBounds(6, 6, 860, 569);
 		
-		frame.getContentPane().add(panelUser);
-		panelUser.setLayout(null);
-		panelSearch.setBounds(0, 0, 352, 68);
+		frame.getContentPane().add(tabbedPane);
 		
-		panelUser.add(panelSearch);
-		panelSearch.setLayout(null);
+		tabbedPane.addTab("Config", null, panelConfig, null);
+		panelConfig.setLayout(null);
+		panelConfigServerUser.setBackground(Color.LIGHT_GRAY);
+		panelConfigServerUser.setBorder(null);
+		panelConfigServerUser.setBounds(201, 21, 448, 278);
 		
-		panelSearch.add(textFieldSearch);
-		btnNewButton_4.setBounds(246, 11, 100, 39);
+		panelConfig.add(panelConfigServerUser);
+		panelConfigServerUser.setLayout(null);
+		btnSave.setBounds(170, 228, 117, 29);
 		
-		panelSearch.add(btnNewButton_4);
-		panelClients.setBounds(10, 74, 332, 543);
+		panelConfigServerUser.add(btnSave);
+		panelConfigServer.setBackground(Color.LIGHT_GRAY);
+		panelConfigServer.setBounds(74, 21, 303, 83);
 		
-		panelUser.add(panelClients);
-		panelClients.setLayout(null);
-		panel_2.setBounds(10, 10, 312, 66);
+		panelConfigServerUser.add(panelConfigServer);
+		panelConfigServer.setLayout(null);
+		lblServerIp.setBounds(23, 27, 63, 16);
+		panelConfigServer.add(lblServerIp);
+		textFieldServerIp.setBounds(108, 22, 139, 26);
+		panelConfigServer.add(textFieldServerIp);
+		textFieldServerIp.setColumns(10);
+		lblServerPort.setBounds(23, 52, 61, 16);
+		panelConfigServer.add(lblServerPort);
+		textFieldServerPort.setBounds(108, 47, 61, 26);
+		panelConfigServer.add(textFieldServerPort);
+		textFieldServerPort.setColumns(10);
+		lblServer.setBounds(23, -1, 61, 16);
 		
-		panelClients.add(panel_2);
-		panel_2.setLayout(null);
-		lblNewLabel_1.setBounds(6, 6, 287, 21);
+		panelConfigServer.add(lblServer);
+		panelConfigUser.setLayout(null);
+		panelConfigUser.setBackground(Color.LIGHT_GRAY);
+		panelConfigUser.setBounds(74, 124, 303, 83);
 		
-		panel_2.add(lblNewLabel_1);
+		panelConfigServerUser.add(panelConfigUser);
+		lblUserIp.setBackground(Color.LIGHT_GRAY);
+		lblUserIp.setBounds(23, 27, 63, 16);
 		
-		panel_2.add(textField_2);
-		panel_2_1.setLayout(null);
-		panel_2_1.setBounds(10, 92, 312, 66);
+		panelConfigUser.add(lblUserIp);
+		textFieldUserIp.setColumns(10);
+		textFieldUserIp.setBounds(108, 22, 139, 26);
 		
-		panelClients.add(panel_2_1);
-		lblNewLabel_1_1.setBounds(6, 6, 287, 21);
+		panelConfigUser.add(textFieldUserIp);
+		lblUserPort.setBounds(23, 52, 61, 16);
 		
-		panel_2_1.add(lblNewLabel_1_1);
-		textField_3.setColumns(10);
-		textField_3.setBounds(6, 28, 287, 32);
+		panelConfigUser.add(lblUserPort);
+		textFieldUserPort.setColumns(10);
+		textFieldUserPort.setBounds(108, 47, 61, 26);
 		
-		panel_2_1.add(textField_3);
-		panel_2_2.setLayout(null);
-		panel_2_2.setBounds(10, 164, 312, 66);
+		panelConfigUser.add(textFieldUserPort);
+		lblUser.setBounds(23, -1, 74, 16);
 		
-		panelClients.add(panel_2_2);
-		lblNewLabel_1_2.setBounds(6, 6, 287, 21);
+		panelConfigUser.add(lblUser);
 		
-		panel_2_2.add(lblNewLabel_1_2);
-		textField_4.setColumns(10);
-		textField_4.setBounds(6, 28, 287, 32);
-		
-		panel_2_2.add(textField_4);
-		panel_2_3.setLayout(null);
-		panel_2_3.setBounds(10, 236, 312, 66);
-		
-		panelClients.add(panel_2_3);
-		lblNewLabel_1_3.setBounds(6, 6, 287, 21);
-		
-		panel_2_3.add(lblNewLabel_1_3);
-		textField_5.setColumns(10);
-		textField_5.setBounds(6, 28, 287, 32);
-		
-		panel_2_3.add(textField_5);
-		panel_2_4.setLayout(null);
-		panel_2_4.setBounds(10, 310, 312, 66);
-		
-		panelClients.add(panel_2_4);
-		lblNewLabel_1_4.setBounds(6, 6, 287, 21);
-		
-		panel_2_4.add(lblNewLabel_1_4);
-		textField_6.setColumns(10);
-		textField_6.setBounds(6, 28, 287, 32);
-		
-		panel_2_4.add(textField_6);
-		panel_2_5.setLayout(null);
-		panel_2_5.setBounds(10, 382, 312, 66);
-		
-		panelClients.add(panel_2_5);
-		lblNewLabel_1_5.setBounds(6, 6, 287, 21);
-		
-		panel_2_5.add(lblNewLabel_1_5);
-		textField_7.setColumns(10);
-		textField_7.setBounds(6, 28, 287, 32);
-		
-		panel_2_5.add(textField_7);
-		panel_2_6.setLayout(null);
-		panel_2_6.setBounds(10, 454, 312, 66);
-		
-		panelClients.add(panel_2_6);
-		lblNewLabel_1_6.setBounds(6, 6, 287, 21);
-		
-		panel_2_6.add(lblNewLabel_1_6);
-		textField_8.setColumns(10);
-		textField_8.setBounds(6, 28, 287, 32);
-		
-		panel_2_6.add(textField_8);
-		panelMessage.setBounds(353, 69, 656, 627);
-		
-		frame.getContentPane().add(panelMessage);
+		tabbedPane.addTab("Chat", null, panelChat, null);
+		panelChat.setLayout(null);
+		panelMessage.setBounds(369, 2, 470, 511);
+		panelChat.add(panelMessage);
 		panelMessage.setLayout(null);
-		panelTextSendAndAdd.setBounds(28, 554, 604, 63);
-		panelMessage.add(panelTextSendAndAdd);
-		panelTextSendAndAdd.setLayout(null);
-		btnNewButton.setBounds(458, 1, 117, 29);
-		panelTextSendAndAdd.add(btnNewButton);
-		btnNewButton_1.setBounds(458, 29, 117, 29);
-		panelTextSendAndAdd.add(btnNewButton_1);
+		panelClientsName.setForeground(Color.LIGHT_GRAY);
+		panelClientsName.setBounds(0, 0, 470, 73);
 		
-		panelTextSendAndAdd.add(textField);
-		panel.setBounds(0, 0, 656, 68);
+		panelMessage.add(panelClientsName);
+		panelClientsName.setLayout(null);
+		lblClientsName.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		lblClientsName.setBounds(0, 0, 470, 67);
+		panelClientsName.add(lblClientsName);
+		panelMessageAddFileAndSend.setBounds(0, 416, 470, 95);
 		
-		panelMessage.add(panel);
-		panel.setLayout(null);
-		lblNewLabel.setBounds(21, 6, 247, 56);
+		panelMessage.add(panelMessageAddFileAndSend);
+		panelMessageAddFileAndSend.setLayout(null);
+		textFieldCurrentMessage.setBounds(12, 6, 302, 83);
+		panelMessageAddFileAndSend.add(textFieldCurrentMessage);
+		textFieldCurrentMessage.setColumns(10);
+		btnFileAdd.setBounds(326, 6, 125, 40);
+		panelMessageAddFileAndSend.add(btnFileAdd);
+		btnMessageSend.setBounds(326, 49, 125, 40);
+		panelMessageAddFileAndSend.add(btnMessageSend);
 		
-		panel.add(lblNewLabel);
-		panel_1.setBounds(28, 74, 604, 474);
-		
-		panelMessage.add(panel_1);
-		panel_1.setLayout(null);
-		
-		panel_1.add(textField_1);
-		btnNewButton_2.addActionListener(new ActionListener() {
+		panelMessage.add(textFieldPreviousMessage);
+		btnMessageSend.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton_2.setBounds(353, 10, 172, 37);
+		panelClients.setBounds(0, 44, 365, 479);
+		panelChat.add(panelClients);
+		panelClients.setLayout(null);
+		scrollPane.setBounds(6, 6, 353, 454);
+		panelClients.add(scrollPane);
+		textFieldUserSearch.setBounds(6, 11, 199, 41);
+		textFieldUserSearch.setColumns(10);
+		scrollPane.setViewportView(panelUserList);
+		panelUserList.setBackground(Color.WHITE);
+		panelUserList.setLayout(null);
+		panelUserSearch.setBackground(Color.WHITE);
+		panelUserSearch.setBounds(0, 0, 349, 68);
 		
-		frame.getContentPane().add(btnNewButton_2);
-		btnNewButton_3.addActionListener(new ActionListener() {
+		panelUserList.add(panelUserSearch);
+		panelUserSearch.setLayout(null);
+		
+		panelUserSearch.add(textFieldUserSearch);
+		btnUserSearch.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton_3.setBounds(166, 10, 183, 37);
+		btnUserSearch.setBounds(217, 13, 100, 39);
 		
-		frame.getContentPane().add(btnNewButton_3);
+		panelUserSearch.add(btnUserSearch);
+		panelUser.setBounds(6, 70, 343, 66);
+		panelUserList.add(panelUser);
+		panelUser.setBackground(Color.LIGHT_GRAY);
+		panelUser.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JOptionPane.showMessageDialog(null,"Test");
+			}
+		});
+		panelUser.setLayout(null);
+		lbClientName.setBounds(6, 6, 287, 21);
 		
-		frame.setJMenuBar(menuBar);
-		btnNewButton.addActionListener(new ActionListener() {
+		panelUser.add(lbClientName);
+		lbClientLastMessage.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
+		lbClientLastMessage.setBounds(16, 29, 61, 16);
+		
+		panelUser.add(lbClientLastMessage);
+		
+		lbClientName.setText(users.get(0).getUsername());
+		panelUser_1.setLayout(null);
+		panelUser_1.setBackground(Color.LIGHT_GRAY);
+		panelUser_1.setBounds(6, 142, 343, 66);
+		
+		panelUserList.add(panelUser_1);
+		lbClientName_1.setBounds(6, 6, 287, 21);
+		
+		panelUser_1.add(lbClientName_1);
+		lbClientLastMessage_1.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
+		lbClientLastMessage_1.setBounds(16, 29, 61, 16);
+		
+		panelUser_1.add(lbClientLastMessage_1);
+		panelUser_2.setLayout(null);
+		panelUser_2.setBackground(Color.LIGHT_GRAY);
+		panelUser_2.setBounds(6, 214, 343, 66);
+		
+		panelUserList.add(panelUser_2);
+		lbClientName_2.setBounds(6, 6, 287, 21);
+		
+		panelUser_2.add(lbClientName_2);
+		lbClientLastMessage_2.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
+		lbClientLastMessage_2.setBounds(16, 29, 61, 16);
+		
+		panelUser_2.add(lbClientLastMessage_2);
+		panelUser_3.setLayout(null);
+		panelUser_3.setBackground(Color.LIGHT_GRAY);
+		panelUser_3.setBounds(6, 286, 343, 66);
+		
+		panelUserList.add(panelUser_3);
+		lbClientName_3.setBounds(6, 6, 287, 21);
+		
+		panelUser_3.add(lbClientName_3);
+		lbClientLastMessage_3.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
+		lbClientLastMessage_3.setBounds(16, 29, 61, 16);
+		
+		panelUser_3.add(lbClientLastMessage_3);
+		btnSelectContacts.setBounds(25, 2, 145, 29);
+		panelChat.add(btnSelectContacts);
+		btnCreateGroup.setBounds(182, 2, 135, 29);
+		btnCreateGroup.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
+		panelChat.add(btnCreateGroup);
+		for(int i = 0; i < users.size(); i++) {
+			panelUser.add(new JLabel(users.get(i).getUsername()));
+		}
+		
 	}
 }
