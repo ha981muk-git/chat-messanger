590ed94388d4d41fb0798929ce23c5ceaab1a363
Kaddour Alnaasan <kaddour@alnaasan.de>
2022-06-19 14:22:30 +0200

    Backend: fix checkstyle-problem

--- src/main/java/edu/fra/uas/net/model/Group.java
+++ src/main/java/edu/fra/uas/net/model/Group.java
@@ -4,32 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author M.Dawoud
+ * This class presents a group of clients
  *
+ * @author M.Dawoud
  */
 public class Group {
-	private String name;
-	private List<User> users = new ArrayList<>();
+    private String name;
+    private List<User> users = new ArrayList<>();
 
-	public Group(String name) {
-		super();
-		this.name = name;
-	}
+    /**
+     * default constructor
+     *
+     * @param name The name of Group
+     */
+    public Group(String name) {
+        super();
+        this.name = name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public List<User> getUsers() {
-		return users;
-	}
+    public List<User> getUsers() {
+        return users;
+    }
 
-	public void setUsers(List<User> users) {
-		this.users = users;
-	}
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
 
 }
