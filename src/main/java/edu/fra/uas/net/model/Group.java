33bd03f6055848158c03c53f2b344209dfefe510
Kaddour Alnaasan <kaddour@alnaasan.de>
2022-06-19 15:54:17 +0200

    Backend: fix checkstyle-problem

--- src/main/java/edu/fra/uas/net/model/Group.java
+++ src/main/java/edu/fra/uas/net/model/Group.java
@@ -4,32 +4,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author M.Dawoud
+ * this class presents Group of Clients
  *
+ * @author M.Dawoud
+ * @author kalnaasan
  */
 public class Group {
-	private String name;
-	private List<User> users = new ArrayList<>();
+    private String name;
+    private List<User> users = new ArrayList<>();
+    private boolean toDelete = false;
 
-	public Group(String name) {
-		super();
-		this.name = name;
-	}
+    /**
+     * default constructor
+     *
+     * @param name name of group
+     */
+    public Group(String name) {
+        super();
+        this.name = name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public List<User> getUsers() {
-		return users;
-	}
+    public List<User> getUsers() {
+        return users;
+    }
 
-	public void setUsers(List<User> users) {
-		this.users = users;
-	}
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
 
+    public boolean isToDelete() {
+        return toDelete;
+    }
+
+    public void setToDelete(boolean toDelete) {
+        this.toDelete = toDelete;
+    }
 }
