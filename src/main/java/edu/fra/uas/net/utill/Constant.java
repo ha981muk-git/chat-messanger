d580f2bf93875ca2bc1b77d60e64bb5b5d759794
Kaddour Alnaasan <kaddour@alnaasan.de>
2022-07-01 23:55:20 +0200

    Backend: add new Constants

--- src/main/java/edu/fra/uas/net/utill/Constant.java
+++ src/main/java/edu/fra/uas/net/utill/Constant.java
@@ -1,19 +1,21 @@
 package edu.fra.uas.net.utill;
 
-import edu.fra.uas.net.gui.Client_old;
-
 import java.util.Random;
 
+/**
+ * in this class all constant variables are defined
+ *
+ * @author kalnaasan
+ */
 public class Constant {
     public static final String SERVE_HOSTNAME = "127.0.0.1";
-    public static final int SERVE_PORT = 8080;
+    public static final int SERVER_PORT = 8080;
     public static final String CLIENT_HOSTNAME = "127.0.0.1";
     public static final int CLIENT_PORT = Constant.generatePort();
     public static final int MIN_PORT = 65000;
     public static final int MAX_PORT = 65100;
-    public static final int SERVER_PORT = 8080;
-    public static final int FRAME_X = 200;
-    public static final int FRAME_Y = 200;
+    public static final int FRAME_X = 400;
+    public static final int FRAME_Y = 300;
     public static final int FRAME_WIDTH = 600;
     public static final int FRAME_HEIGHT = 340;
     public static final int BORDER = 5;
@@ -22,6 +24,15 @@ public class Constant {
     public static final int BUTTON_DELETE_Y = 165;
     public static final int SCROLL_TAB_PANE_HEIGHT = 149;
     public static final int SCROLL_TAB_PANE_WIDTH = 549;
+    public static final int CLIENT_GUI_WIDTH = 860;
+    public static final int CLIENT_GUI_HEIGHT = 625;
+    public static final int COMPONENT_X5 = 5;
+    public static final int COMPONENT_X10 = 10;
+    public static final int COMPONENT_Y5 = 5;
+    public static final int CLIENT_LIST_CLIENTS_WIDTH = 190;
+
+    private Constant() {
+    }
 
     /**
      * to generate port of client
